96ac3a1160cfd6e0caf84d056e154a37d6c0360b
conner fortner <fortner.conner@yahoo.com>
2020-12-02 20:13:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+
 	get '/' do
-	        erb :index
+		erb :index
 	end
-	
-	get "/info" do
-      "Testing the info page"
-  end
+
+	get '/info' do
+		erb :info
+	end 
 end
